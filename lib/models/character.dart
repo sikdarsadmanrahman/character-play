ad2e8e74089229b0a06a9d75ab4302bbcb4354d3
sikdarsadmanrahman <sikdarsadmanrahman@gmail.com>
2026-02-20 03:06:51 +0600

    adding images

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,27 +1,38 @@
-class Character {
+import 'package:character_game/models/skill.dart';
+import 'package:character_game/models/stats.dart';
+import 'package:character_game/models/vocation.dart';
+
+class Character with Stats{
 
   // constructor
   Character({
     required this.id,
     required this.name,
     required this.slogan,
+    required this.vocation,
   });
 
 
   //fields
+  final Set<Skill> skills = {};
   final String id;
   final String name;
   final String slogan;
+  final Vocation vocation;
   bool _isFav = false;
 
   //getter
   bool get isFav => _isFav;
 
-  //function
+  //methods
   void toggleFav(){
     _isFav = !_isFav;
   }
 
+  void updateSkills(Skill skill) {
+    skills.clear();
+    skills.add(skill);
+  }
 
 }
 
